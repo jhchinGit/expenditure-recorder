d6606d956066a95ac7c75efcd0a8cc05d9c21081
JHSW <50164113+jhchinGit@users.noreply.github.com>
2022-09-05 23:48:02 +0800

    Update

--- ExpenditureRecorder.Services/RecorderService.cs
+++ ExpenditureRecorder.Services/RecorderService.cs
@@ -13,24 +13,6 @@ namespace ExpenditureRecorder.Services
             _recorder = new Recorder(budget, DateTime.Now.Date);
         }
 
-        public void SetBudget(decimal budget)
-        {
-            SetBudget(budget, 100, BudgetTimeline.Monthly);
-        }
-
-        public void SetBudget(decimal budget, int alertPercentage)
-        {
-            SetBudget(budget, alertPercentage, BudgetTimeline.Monthly);
-        }
-
-        public void SetBudget(decimal budget, BudgetTimeline timeline)
-        {
-            SetBudget(budget, 100, timeline);
-        }
-
-        public void SetBudget(decimal budget, int alertPercentage, BudgetTimeline timeline)
-        {
-            _recorder.Budget = new Budget(budget, alertPercentage, timeline);
-        }
+        
     }
 }
